a5ed28f96721f84083e30d04b63e0a44a6dcbeb0
luomvv1 <128294772+luomvv1@users.noreply.github.com>
2025-08-22 20:43:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from DigitalOcean App Platform!"
+    return "Hello from DigitalOcean Toi la Vo Van Luom"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
